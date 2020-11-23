9d62c3db60a33f8a38af7b6ac62d612c57bddf82
教瘦 <54337467@qq.com>
2020-11-23 09:55:14 +0800

    路由切换主内容区域

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
         <Sidebar></Sidebar>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <!-- 主内容区域 -->
-          <Heroes></Heroes>
+          <router-view></router-view>
         </div>
       </div>
     </div>
@@ -19,13 +19,11 @@
 // 导入 Header 组件
 import Header from './components/Header'
 import Sidebar from './components/Sidebar'
-import Heroes from './components/Heroes'
 export default {
   // 注册局部组件
   components: {
     Header,
-    Sidebar,
-    Heroes
+    Sidebar
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
+// Vue.use() 注册Vue的插件
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/heroes',
+    name: 'heroes',
+    component: () => import(/* webpackChunkName: "heroes" */ '../views/heroes/List.vue')
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/weapons',
+    name: 'weapons',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "weapons" */ '../views/weapons/List.vue')
+  },
+  {
+    path: '/equips',
+    name: 'equips',
+    component: () => import(/* webpackChunkName: "equips" */ '../views/equips/List.vue')
   }
 ]
 
