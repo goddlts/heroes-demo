fbb0e9d55327fd1b49a329d825611a8eb5c06442
教瘦 <54337467@qq.com>
2020-11-23 09:27:25 +0800

    拆分Header和Sidebar组件

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,241 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <!-- Header -->
+    <Header></Header>
+    <div class="container-fluid">
+      <div class="row">
+        <!-- Sidebar -->
+        <Sidebar></Sidebar>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h2 class="sub-header">Hero List</h2>
+          <a class="btn btn-success" href="add.html">Add</a>
+          <div class="table-responsive">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                  <th>操作</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1,001</td>
+                  <td>Lorem</td>
+                  <td>ipsum</td>
+                  <td>dolor</td>
+                  <td>sit</td>
+                  <td>
+                    <a href="edit.html">edit</a>
+                    &nbsp;&nbsp;
+                    <a href="javascript:window.confirm('Are you sure?')">delete</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1,002</td>
+                  <td>amet</td>
+                  <td>consectetur</td>
+                  <td>adipiscing</td>
+                  <td>elit</td>
+                  <td>
+                    <a href="edit.html">edit</a>
+                    &nbsp;&nbsp;
+                    <a href="javascript:window.confirm('Are you sure?')">delete</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1,003</td>
+                  <td>Integer</td>
+                  <td>nec</td>
+                  <td>odio</td>
+                  <td>Praesent</td>
+                  <td>
+                    <a href="edit.html">edit</a>
+                    &nbsp;&nbsp;
+                    <a href="javascript:window.confirm('Are you sure?')">delete</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1,003</td>
+                  <td>libero</td>
+                  <td>Sed</td>
+                  <td>cursus</td>
+                  <td>ante</td>
+                  <td>
+                    <a href="edit.html">edit</a>
+                    &nbsp;&nbsp;
+                    <a href="javascript:window.confirm('Are you sure?')">delete</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1,004</td>
+                  <td>dapibus</td>
+                  <td>diam</td>
+                  <td>Sed</td>
+                  <td>nisi</td>
+                  <td>
+                    <a href="edit.html">edit</a>
+                    &nbsp;&nbsp;
+                    <a href="javascript:window.confirm('Are you sure?')">delete</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1,005</td>
+                  <td>Nulla</td>
+                  <td>quis</td>
+                  <td>sem</td>
+                  <td>at</td>
+                  <td>
+                    <a href="edit.html">edit</a>
+                    &nbsp;&nbsp;
+                    <a href="javascript:window.confirm('Are you sure?')">delete</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1,006</td>
+                  <td>nibh</td>
+                  <td>elementum</td>
+                  <td>imperdiet</td>
+                  <td>Duis</td>
+                  <td>
+                    <a href="edit.html">edit</a>
+                    &nbsp;&nbsp;
+                    <a href="javascript:window.confirm('Are you sure?')">delete</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1,007</td>
+                  <td>sagittis</td>
+                  <td>ipsum</td>
+                  <td>Praesent</td>
+                  <td>mauris</td>
+                  <td>
+                    <a href="edit.html">edit</a>
+                    &nbsp;&nbsp;
+                    <a href="javascript:window.confirm('Are you sure?')">delete</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1,008</td>
+                  <td>Fusce</td>
+                  <td>nec</td>
+                  <td>tellus</td>
+                  <td>sed</td>
+                  <td>
+                    <a href="edit.html">edit</a>
+                    &nbsp;&nbsp;
+                    <a href="javascript:window.confirm('Are you sure?')">delete</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1,009</td>
+                  <td>augue</td>
+                  <td>semper</td>
+                  <td>porta</td>
+                  <td>Mauris</td>
+                  <td>
+                    <a href="edit.html">edit</a>
+                    &nbsp;&nbsp;
+                    <a href="javascript:window.confirm('Are you sure?')">delete</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1,010</td>
+                  <td>massa</td>
+                  <td>Vestibulum</td>
+                  <td>lacinia</td>
+                  <td>arcu</td>
+                  <td>
+                    <a href="edit.html">edit</a>
+                    &nbsp;&nbsp;
+                    <a href="javascript:window.confirm('Are you sure?')">delete</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1,011</td>
+                  <td>eget</td>
+                  <td>nulla</td>
+                  <td>Class</td>
+                  <td>aptent</td>
+                  <td>
+                    <a href="edit.html">edit</a>
+                    &nbsp;&nbsp;
+                    <a href="javascript:window.confirm('Are you sure?')">delete</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1,012</td>
+                  <td>taciti</td>
+                  <td>sociosqu</td>
+                  <td>ad</td>
+                  <td>litora</td>
+                  <td>
+                    <a href="edit.html">edit</a>
+                    &nbsp;&nbsp;
+                    <a href="javascript:window.confirm('Are you sure?')">delete</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1,013</td>
+                  <td>torquent</td>
+                  <td>per</td>
+                  <td>conubia</td>
+                  <td>nostra</td>
+                  <td>
+                    <a href="edit.html">edit</a>
+                    &nbsp;&nbsp;
+                    <a href="javascript:window.confirm('Are you sure?')">delete</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1,014</td>
+                  <td>per</td>
+                  <td>inceptos</td>
+                  <td>himenaeos</td>
+                  <td>Curabitur</td>
+                  <td>
+                    <a href="edit.html">edit</a>
+                    &nbsp;&nbsp;
+                    <a href="javascript:window.confirm('Are you sure?')">delete</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>1,015</td>
+                  <td>sodales</td>
+                  <td>ligula</td>
+                  <td>in</td>
+                  <td>libero</td>
+                  <td>
+                    <a href="edit.html">edit</a>
+                    &nbsp;&nbsp;
+                    <a href="javascript:window.confirm('Are you sure?')">delete</a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+// 导入 Header 组件
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+export default {
+  // 注册局部组件
+  components: {
+    Header,
+    Sidebar
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
+// 导入一个模块
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 引入 node_modules 中的内容，不写 ./
+import 'bootstrap/dist/css/bootstrap.css'
+// 引入当前项目中的内容要写 ./
+import './assets/css/index.css'
+// 如果 import 的路径中不写 ./ 默认回去 node_moduels 中加载内容
 
 Vue.config.productionTip = false
 
